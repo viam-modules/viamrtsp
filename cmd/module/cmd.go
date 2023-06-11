93e9516dd608af435b579185a228172f2c973ab1
Eliot Horowitz <eliothorowitz@gmail.com>
2023-06-11 12:12:41 -0400

    module

---
+++ cmd/module/cmd.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+
+	"github.com/edaniels/golog"
+
+	"go.viam.com/rdk/components/camera"
+	"go.viam.com/rdk/module"
+
+	"github.com/erh/viamrtsp"
+)
+
+func main() {
+	err := realMain()
+	if err != nil {
+		panic(err)
+	}
+}
+func realMain() error {
+
+	ctx := context.Background()
+	logger := golog.NewDevelopmentLogger("client")
+
+	myMod, err := module.NewModuleFromArgs(ctx, logger)
+	if err != nil {
+		return err
+	}
+
+	err = myMod.AddModelFromRegistry(ctx, camera.API, viamrtsp.ModelH264)
+	if err != nil {
+		return err
+	}
+
+	err = myMod.Start(ctx)
+	defer myMod.Close(ctx)
+	if err != nil {
+		return err
+	}
+	<-ctx.Done()
+	return nil
+}
