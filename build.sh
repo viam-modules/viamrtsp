2ff20ea89f017834ef91d30c855a1b38d0957c58
hexbabe <sean.xiaochen.yu@gmail.com>
2024-05-28 13:07:21 -0400

    Add deps apt install

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -e
 
+sudo apt-get update
+sudo apt-get install -y libfuse2 ffmpeg pkg-config
+
 make module
-OS=$(uname -s)
-mv ./bin/$OS/module.tar.gz .
+
+UNAME_S=$(uname -s)
+UNAME_M=$(uname -m)
+
+output_dir="./bin/${UNAME_S}-${UNAME_M}"
+archive_name="module.tar.gz"
+
+if [ -f "${output_dir}/${archive_name}" ]; then 
+    mkdir -p ./dist
+    mv "${output_dir}/${archive_name}" "./dist/archive.tar.gz"
+    echo "Moved ${archive_name} to the current directory."
+else
+    echo "Error: ${archive_name} not found in ${output_dir}." >&2
+    exit 1
+fi
