10b4d343fdb79993712ff050dc19988f8d8bb12a
seanavery <seanpollock3344@gmail.com>
2025-01-14 15:35:56 -0500

    Separate out encoder init into helper

--- mime.go
+++ mime.go
@@ -36,43 +36,9 @@ func newMimeHandler(logger logging.Logger) *mimeHandler {
 
 func (mh *mimeHandler) convertJPEG(frame *avFrameWrapper) ([]byte, camera.ImageMetadata, error) {
 	if mh.jpegEnc == nil || frame.frame.width != mh.jpegEnc.width || frame.frame.height != mh.jpegEnc.height {
-		// Lock to prevent modifying encoder while it is being used concurrently.
-		// Frame param changes are rare, so we can afford to block here.
-		mh.mu.Lock()
-		mh.logger.Info("creating MJPEG encoder with frame size: ", frame.frame.width, "x", frame.frame.height)
-		if mh.jpegEnc != nil {
-			C.avcodec_free_context(&mh.jpegEnc)
+		if err := mh.initJPEGEncoder(frame); err != nil {
+			return nil, camera.ImageMetadata{}, err
 		}
-		codec := C.avcodec_find_encoder(C.AV_CODEC_ID_MJPEG)
-		if codec == nil {
-			mh.mu.Unlock()
-			return nil, camera.ImageMetadata{}, errors.New("failed to find MJPEG encoder")
-		}
-		mh.jpegEnc = C.avcodec_alloc_context3(codec)
-		mh.jpegEnc.width = frame.frame.width
-		mh.jpegEnc.height = frame.frame.height
-		mh.jpegEnc.pix_fmt = C.AV_PIX_FMT_YUVJ420P
-		// We don't care about accurate timestamps still frames
-		mh.jpegEnc.time_base = C.AVRational{num: 1, den: 1}
-		var opts *C.AVDictionary
-		// Equivalent to 75% quality
-		qscaleKey := C.CString("qscale")
-		qscaleValue := C.CString("8")
-		defer func() {
-			C.free(unsafe.Pointer(qscaleValue))
-			C.free(unsafe.Pointer(qscaleKey))
-			C.av_dict_free(&opts)
-		}()
-		res := C.av_dict_set(&opts, qscaleKey, qscaleValue, 0)
-		if res < 0 {
-			mh.mu.Unlock()
-			return nil, camera.ImageMetadata{}, newAvError(res, "failed to set qscale option")
-		}
-		if res := C.avcodec_open2(mh.jpegEnc, codec, nil); res < 0 {
-			mh.mu.Unlock()
-			return nil, camera.ImageMetadata{}, newAvError(res, "failed to open MJPEG encoder")
-		}
-		mh.mu.Unlock()
 	}
 	if mh.jpegEnc == nil {
 		return nil, camera.ImageMetadata{}, errors.New("failed to create encoder or destination frame")
@@ -100,6 +66,45 @@ func (mh *mimeHandler) convertJPEG(frame *avFrameWrapper) ([]byte, camera.ImageM
 	return dataGo, camera.ImageMetadata{
 		MimeType: rutils.MimeTypeJPEG,
 	}, nil
+}
+
+func (mh *mimeHandler) initJPEGEncoder(frame *avFrameWrapper) error {
+	// Lock to prevent modifying encoder while it is being used concurrently.
+	// Frame param changes are rare, so we can afford to block here.
+	mh.mu.Lock()
+	defer mh.mu.Unlock()
+	mh.logger.Info("creating MJPEG encoder with frame size: ", frame.frame.width, "x", frame.frame.height)
+	if mh.jpegEnc != nil {
+		C.avcodec_free_context(&mh.jpegEnc)
+	}
+	codec := C.avcodec_find_encoder(C.AV_CODEC_ID_MJPEG)
+	if codec == nil {
+		return errors.New("failed to find MJPEG encoder")
+	}
+	mh.jpegEnc = C.avcodec_alloc_context3(codec)
+	mh.jpegEnc.width = frame.frame.width
+	mh.jpegEnc.height = frame.frame.height
+	mh.jpegEnc.pix_fmt = C.AV_PIX_FMT_YUVJ420P
+	// We don't care about accurate timestamps still frames
+	mh.jpegEnc.time_base = C.AVRational{num: 1, den: 1}
+	var opts *C.AVDictionary
+	// Equivalent to 75% quality
+	qscaleKey := C.CString("qscale")
+	qscaleValue := C.CString("8")
+	defer func() {
+		C.free(unsafe.Pointer(qscaleValue))
+		C.free(unsafe.Pointer(qscaleKey))
+		C.av_dict_free(&opts)
+	}()
+	res := C.av_dict_set(&opts, qscaleKey, qscaleValue, 0)
+	if res < 0 {
+		return newAvError(res, "failed to set qscale option")
+	}
+	if res := C.avcodec_open2(mh.jpegEnc, codec, nil); res < 0 {
+		return newAvError(res, "failed to open MJPEG encoder")
+	}
+
+	return nil
 }
 
 func (mh *mimeHandler) close() {
