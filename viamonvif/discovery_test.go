76ef8b74b0fe1356d9a0f7caa4406a91277de4a4
Sean Pollock <seanpollock3344@gmail.com>
2025-04-21 13:38:19 -0400

    [RSDK-10433] - Add preview do command (#122)
    * Working snapshot discovery flow
    
    * Pass in token instead of profile
    
    * Refactor stream info lookup
    
    * Remove snapshot_uri from config
    
    * Switch to attribute input
    
    * Wokring data url
    
    * Lint happy
    
    * Honest attempt to get basic auth working
    
    * Use external digest auth dep
    
    * Add debug logs to http client
    
    * Add helper fn for data url formatting
    
    * Add snapshot service test cases
    
    * Simplify bad status code handling
    
    * Add mock snapshot server service tests
    
    * Lint happy
    
    * Add snapshot uri checks to discovery tests
    
    * Add preview documentation
    
    * Add locking mechanism to rtspDiscovery struct
    
    * Handle missing snapshot uri
    
    * Fix mdns uri lookup
    
    * Clean
    
    * Rename MediaInfo
    
    * Use a map for snapshot uri lookup
    
    * Clean up
    
    * Remove todo comments
    
    * Bump rdk dep
    
    * Remove rtsp config change
    
    * Copilot review
    
    * Simplify preview doc
    
    * Improve discovery logging
    
    * Clean up comment
    
    * Clean up logging for uri fetchers
    
    * Remove full data url debug log
    
    * Return body response when bad status code
    
    * Succeed even if snapshot uri fetch fails
    
    * Remove extra nil check
    
    * Check for password lookup failure
    
    * Remove nil check
    
    * Make http client timeout a const
    
    * Use httptest package to test out self signed cert https snapshot uris
    
    * Improve fn comments
    
    * Sp
    
    * Sp
    
    * Quick fix preview download
    
    * Do not truncate body text
    
    * Remove empty stream uri check
    
    * Make snapshot map private
    
    * Improve locking
    
    * Quick fix
    
    * Remove profile debug log
    
    * Remove extra empty check
    
    Co-authored-by: nicksanford <nicholascsanford@gmail.com>
    
    * Lint
    
    * Sp
    
    * Bump api dep
    
    * Reuse no cams found err cosnt
    
    * Nit fixes
    
    ---------
    
    Co-authored-by: nicksanford <nicholascsanford@gmail.com>

--- viamonvif/discovery_test.go
+++ viamonvif/discovery_test.go
@@ -43,8 +43,12 @@ func (m *MockDevice) GetProfiles(_ context.Context) (device.GetProfilesResponse,
 	}, nil
 }
 
-func (m *MockDevice) GetStreamURI(_ context.Context, profile onvif.Profile, creds device.Credentials) (*url.URL, error) {
-	if profile.Token != "profile1" || profile.Name != "Main Profile" {
+func (m *MockDevice) GetSnapshotURI(_ context.Context, _ onvif.ReferenceToken, _ device.Credentials) (*url.URL, error) {
+	return url.Parse("http://example.com/snapshot.jpg")
+}
+
+func (m *MockDevice) GetStreamURI(_ context.Context, token onvif.ReferenceToken, creds device.Credentials) (*url.URL, error) {
+	if token != "profile1" {
 		return nil, errors.New("invalid mock profile")
 	}
 	u, err := url.Parse("rtsp://192.168.1.100/stream")
@@ -69,25 +73,27 @@ func TestGetCameraInfo(t *testing.T) {
 		test.That(t, cameraInfo.Manufacturer, test.ShouldEqual, "Evil Inc.")
 		test.That(t, cameraInfo.Model, test.ShouldEqual, "Doom Ray Camera of Certain Annihilation")
 		test.That(t, cameraInfo.SerialNumber, test.ShouldEqual, "44444444")
-		test.That(t, len(cameraInfo.RTSPURLs), test.ShouldEqual, 1)
-		test.That(t, cameraInfo.RTSPURLs[0], test.ShouldEqual, "rtsp://username:password@192.168.1.100/stream")
+		test.That(t, len(cameraInfo.MediaEndpoints), test.ShouldEqual, 1)
+		test.That(t, cameraInfo.MediaEndpoints[0].StreamURI, test.ShouldEqual, "rtsp://username:password@192.168.1.100/stream")
 		test.That(t, cameraInfo.Name(0), test.ShouldEqual, "EvilInc-DoomRayCameraofCertainAnnihilation-44444444-url0")
 
-		t.Run("GetRTSPStreamURLs with credentials", func(t *testing.T) {
+		t.Run("GetCameraInfo with credentials", func(t *testing.T) {
 			uri, err := url.Parse("192.168.1.100")
 			test.That(t, err, test.ShouldBeNil)
 			cameraInfo, err := GetCameraInfo(context.Background(), mockDevice, uri, device.Credentials{User: "username", Pass: "password"}, logger)
 			test.That(t, err, test.ShouldBeNil)
-			test.That(t, len(cameraInfo.RTSPURLs), test.ShouldEqual, 1)
-			test.That(t, cameraInfo.RTSPURLs[0], test.ShouldEqual, "rtsp://username:password@192.168.1.100/stream")
+			test.That(t, len(cameraInfo.MediaEndpoints), test.ShouldEqual, 1)
+			test.That(t, cameraInfo.MediaEndpoints[0].StreamURI, test.ShouldEqual, "rtsp://username:password@192.168.1.100/stream")
+			test.That(t, cameraInfo.MediaEndpoints[0].SnapshotURI, test.ShouldEqual, "http://example.com/snapshot.jpg")
 		})
-		t.Run("GetRTSPStreamURLs without credentials", func(t *testing.T) {
+		t.Run("GetCameraInfo without credentials", func(t *testing.T) {
 			uri, err := url.Parse("192.168.1.100")
 			test.That(t, err, test.ShouldBeNil)
 			cameraInfo, err := GetCameraInfo(context.Background(), mockDevice, uri, device.Credentials{}, logger)
 			test.That(t, err, test.ShouldBeNil)
-			test.That(t, len(cameraInfo.RTSPURLs), test.ShouldEqual, 1)
-			test.That(t, cameraInfo.RTSPURLs[0], test.ShouldEqual, "rtsp://192.168.1.100/stream")
+			test.That(t, len(cameraInfo.MediaEndpoints), test.ShouldEqual, 1)
+			test.That(t, cameraInfo.MediaEndpoints[0].StreamURI, test.ShouldEqual, "rtsp://192.168.1.100/stream")
+			test.That(t, cameraInfo.MediaEndpoints[0].SnapshotURI, test.ShouldEqual, "http://example.com/snapshot.jpg")
 		})
 	})
 }
--- viamonvif/service.go
+++ viamonvif/service.go
@@ -3,13 +3,21 @@ package viamonvif
 
 import (
 	"context"
+	"crypto/tls"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
+	"time"
 
+	"github.com/icholy/digest"
 	"github.com/viam-modules/viamrtsp"
 	"github.com/viam-modules/viamrtsp/viamonvif/device"
 	"go.viam.com/rdk/components/camera"
@@ -23,6 +31,10 @@ var (
 	Model             = viamrtsp.Family.WithModel("onvif")
 	errNoCamerasFound = errors.New("no cameras found, ensure cameras are working or check credentials")
 	emptyCred         = device.Credentials{}
+)
+
+const (
+	snapshotClientTimeout = 5 * time.Second
 )
 
 func init() {
@@ -50,9 +62,17 @@ func (cfg *Config) Validate(_ string) ([]string, error) {
 	return []string{}, nil
 }
 
+type previewRequest struct {
+	rtspURL string
+}
+
 type rtspDiscovery struct {
 	resource.Named
 	resource.AlwaysRebuild
+
+	rtspToSnapshotURIsMu sync.Mutex
+	rtspToSnapshotURIs   map[string]string
+
 	Credentials []device.Credentials
 	mdnsServer  *mdnsServer
 	logger      logging.Logger
@@ -88,6 +108,7 @@ func newDiscovery(_ context.Context, _ resource.Dependencies,
 
 // DiscoverResources discovers different rtsp cameras that use onvif.
 func (dis *rtspDiscovery) DiscoverResources(ctx context.Context, extra map[string]any) ([]resource.Config, error) {
+	localRTSPToSnapshotURIs := make(map[string]string)
 	cams := []resource.Config{}
 
 	discoverCreds := dis.Credentials
@@ -101,13 +122,13 @@ func (dis *rtspDiscovery) DiscoverResources(ctx context.Context, extra map[strin
 		return nil, err
 	}
 	if len(list.Cameras) == 0 {
-		return nil, errors.New("no cameras found, ensure cameras are working or check credentials")
+		return nil, errNoCamerasFound
 	}
 
 	for _, camInfo := range list.Cameras {
 		dis.logger.Debugf("%s %s %s", camInfo.Manufacturer, camInfo.Model, camInfo.SerialNumber)
 		// some cameras return with no urls. explicitly skipping those so the behavior is clear in the service.
-		if len(camInfo.RTSPURLs) == 0 {
+		if len(camInfo.MediaEndpoints) == 0 {
 			dis.logger.Errorf("No urls found for camera, skipping. %s %s %s",
 				camInfo.Manufacturer, camInfo.Model, camInfo.SerialNumber)
 			continue
@@ -123,12 +144,56 @@ func (dis *rtspDiscovery) DiscoverResources(ctx context.Context, extra map[strin
 		if err != nil {
 			return nil, err
 		}
+		for _, endpoint := range camInfo.MediaEndpoints {
+			// If available, we will use mdns rtsp address as the key instead of the original rtsp address
+			localRTSPToSnapshotURIs[endpoint.StreamURI] = endpoint.SnapshotURI
+			dis.logger.Debugf("Added snapshot mapping: %s - %s", endpoint.StreamURI, endpoint.SnapshotURI)
+		}
 		cams = append(cams, camConfigs...)
 	}
+
+	// Only lock when updating the shared URI map
+	dis.rtspToSnapshotURIsMu.Lock()
+	dis.rtspToSnapshotURIs = localRTSPToSnapshotURIs
+	dis.rtspToSnapshotURIsMu.Unlock()
 
 	dis.mdnsServer.UpdateCacheFile()
 
 	return cams, nil
+}
+
+func (dis *rtspDiscovery) DoCommand(ctx context.Context, command map[string]interface{}) (map[string]interface{}, error) {
+	cmd, ok := command["command"].(string)
+	if !ok {
+		return nil, errors.New("invalid command type")
+	}
+
+	switch cmd {
+	case "preview":
+		dis.logger.Debugf("snapshot command received")
+		previewReq, err := toPreviewCommand(command)
+		if err != nil {
+			return nil, err
+		}
+		dis.rtspToSnapshotURIsMu.Lock()
+		snapshotURI, found := dis.rtspToSnapshotURIs[previewReq.rtspURL]
+		dis.rtspToSnapshotURIsMu.Unlock()
+		if !found {
+			return nil, fmt.Errorf("snapshot URI not found for %s", previewReq.rtspURL)
+		}
+		dis.logger.Infof("snapshot URI: %s", snapshotURI)
+
+		dataURL, err := downloadPreviewImage(ctx, dis.logger, snapshotURI)
+		if err != nil {
+			return nil, fmt.Errorf("failed to download preview image: %w", err)
+		}
+		return map[string]interface{}{
+			"preview": dataURL,
+		}, nil
+
+	default:
+		return nil, fmt.Errorf("unknown command: %s", cmd)
+	}
 }
 
 func (dis *rtspDiscovery) Close(_ context.Context) error {
@@ -136,9 +201,95 @@ func (dis *rtspDiscovery) Close(_ context.Context) error {
 	return nil
 }
 
+func toPreviewCommand(command map[string]interface{}) (*previewRequest, error) {
+	attributes, ok := command["attributes"].(map[string]interface{})
+	if !ok {
+		return nil, errors.New("attributes is missing or not a map")
+	}
+	rtspURL, ok := attributes["rtsp_address"].(string)
+	if !ok {
+		return nil, errors.New("rtsp_address cannot be empty")
+	}
+	return &previewRequest{rtspURL: rtspURL}, nil
+}
+
+// formatDataURL formats the image data and content type into a data URL.
+func formatDataURL(contentType string, imageBytes []byte) string {
+	base64Image := base64.StdEncoding.EncodeToString(imageBytes)
+	return fmt.Sprintf("data:%s;base64,%s", contentType, base64Image)
+}
+
+// downloadPreviewImage downloads the preview image from the snapshot uri and returns it as a data URL.
+func downloadPreviewImage(ctx context.Context, logger logging.Logger, snapshotURI string) (string, error) {
+	parsedURL, err := url.Parse(snapshotURI)
+	if err != nil {
+		return "", fmt.Errorf("found an invalid snapshot URI: %w", err)
+	}
+
+	var username, password string
+	if parsedURL.User != nil {
+		username = parsedURL.User.Username()
+		if pwd, hasPassword := parsedURL.User.Password(); hasPassword {
+			password = pwd
+		}
+		if password == "" {
+			logger.Warnf("found a snapshot URI with no password: %s", snapshotURI)
+		}
+		logger.Debugf("creating snapshot request using credentials: username=%s", username)
+	}
+
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, //nolint:gosec
+	}
+	client := &http.Client{
+		// Setting upper bound timeout in case the ctx never times out
+		Timeout: snapshotClientTimeout,
+		Transport: &digest.Transport{
+			Username:  username,
+			Password:  password,
+			Transport: transport,
+		},
+	}
+
+	getReq, err := http.NewRequestWithContext(ctx, http.MethodGet, snapshotURI, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create http request: %w", err)
+	}
+
+	resp, err := client.Do(getReq)
+	if err != nil {
+		return "", fmt.Errorf("failed to execute http request: %w", err)
+	}
+	defer resp.Body.Close()
+	logger.Debugf("snapshot response status: %s", resp.Status)
+
+	if resp.StatusCode != http.StatusOK {
+		statusText := http.StatusText(resp.StatusCode)
+		bodyText := "<could not read response body>"
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err == nil {
+			bodyText = string(bodyBytes)
+		} else {
+			logger.Warnf("failed to read error response body: %v", err)
+		}
+		return "", fmt.Errorf("failed to get snapshot image, status %d: %s, body: %s", resp.StatusCode, statusText, bodyText)
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	imageBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read image data from http response: %w", err)
+	}
+	logger.Debugf("retrieved image data: %d bytes and content type: %s", len(imageBytes), contentType)
+
+	dataURL := formatDataURL(contentType, imageBytes)
+
+	return dataURL, nil
+}
+
 func createCamerasFromURLs(l CameraInfo, discoveryDependencyName string, logger logging.Logger) ([]resource.Config, error) {
 	cams := []resource.Config{}
-	for index, u := range l.RTSPURLs {
+	for index, u := range l.MediaEndpoints {
 		logger.Debugf("camera URL:\t%s", u)
 
 		// Some URLs may contain a hostname that is served by the DiscoveryService's mDNS
@@ -149,7 +300,7 @@ func createCamerasFromURLs(l CameraInfo, discoveryDependencyName string, logger 
 			discDep = discoveryDependencyName
 		}
 
-		cfg, err := createCameraConfig(l.Name(index), u, discDep)
+		cfg, err := createCameraConfig(l.Name(index), u.StreamURI, discDep)
 		if err != nil {
 			return nil, err
 		}
