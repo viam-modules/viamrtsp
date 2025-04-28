68bfcf2054490eb13018cb8aa015a95b1be6d50a
seanavery <seanpollock3344@gmail.com>
2025-04-28 13:30:24 -0400

    POC pan tilt keyboard ctrls

---
+++ examples/ptz-ctrls/client.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"go.viam.com/rdk/components/camera"
+	"go.viam.com/rdk/components/generic"
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/robot/client"
+	"go.viam.com/utils/rpc"
+
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
+)
+
+func main() {
+	logger := logging.NewDebugLogger("client")
+	machine, err := client.New(
+		context.Background(),
+		"<MACHINE_ADDRESS>",
+		logger,
+		client.WithDialOptions(rpc.WithEntityCredentials(
+			/* Replace "<API-KEY-ID>" (including brackets) with your machine's API key ID */
+			"<API-KEY-ID>",
+			rpc.Credentials{
+				Type: rpc.CredentialsTypeAPIKey,
+				/* Replace "<API-KEY>" (including brackets) with your machine's API key */
+				Payload: "<API-KEY>",
+			})),
+	)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer machine.Close(context.Background())
+	logger.Info("Resources:")
+	logger.Info(machine.ResourceNames())
+
+	// rtsp-cam-1
+	rtspCam1, err := camera.FromRobot(machine, "rtsp-cam-1")
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	rtspCam1ReturnValue, err := rtspCam1.Properties(context.Background())
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	logger.Infof("rtsp-cam-1 Properties return value: %+v", rtspCam1ReturnValue)
+
+	ptz, err := generic.FromRobot(machine, "ptz-1")
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+
+	// Stop any ongoing pan/tilt/zoom commands
+	time.Sleep(1 * time.Second)
+	ptzReturnValue, err := ptz.DoCommand(
+		context.Background(),
+		map[string]interface{}{
+			"command":  "stop",
+			"pan_tilt": true,
+			"zoom":     true,
+		},
+	)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	logger.Infof("ptz-1 DoCommand return value: %+v", ptzReturnValue)
+
+	pressed := make(map[string]*time.Timer) // Use string as the key type
+	var mu sync.Mutex
+
+	// Duration after which we consider the key "released"
+	releaseDelay := 100 * time.Millisecond
+
+	keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+		mu.Lock()
+		defer mu.Unlock()
+
+		keyStr := key.String() // Convert the key to a string
+		fmt.Printf("Key pressed: %s\n", keyStr)
+
+		// If a timer exists for this key, stop and reset it
+		if t, ok := pressed[keyStr]; ok {
+			t.Stop()
+		}
+
+		// Handle pan and tilt commands for WASD keys
+		switch keyStr {
+		case "W", "w":
+			go func() {
+				_, err := ptz.DoCommand(context.Background(), map[string]interface{}{
+					"command":    "continuous-move",
+					"pan_speed":  0.0,
+					"tilt_speed": 0.5,
+				})
+				if err != nil {
+					logger.Error(err)
+				}
+			}()
+		case "S", "s":
+			go func() {
+				_, err := ptz.DoCommand(context.Background(), map[string]interface{}{
+					"command":    "continuous-move",
+					"pan_speed":  0.0,
+					"tilt_speed": -0.5,
+				})
+				if err != nil {
+					logger.Error(err)
+				}
+			}()
+		case "A", "a":
+			go func() {
+				_, err := ptz.DoCommand(context.Background(), map[string]interface{}{
+					"command":    "continuous-move",
+					"pan_speed":  -0.5,
+					"tilt_speed": 0.0,
+				})
+				if err != nil {
+					logger.Error(err)
+				}
+			}()
+		case "D", "d":
+			go func() {
+				_, err := ptz.DoCommand(context.Background(), map[string]interface{}{
+					"command":    "continuous-move",
+					"pan_speed":  0.5,
+					"tilt_speed": 0.0,
+				})
+				if err != nil {
+					logger.Error(err)
+				}
+			}()
+
+		default:
+			fmt.Printf("Key pressed: %s\n", keyStr)
+		}
+
+		// Start a timer to emulate "key release"
+		pressed[keyStr] = time.AfterFunc(releaseDelay, func() {
+			mu.Lock()
+			defer mu.Unlock()
+			fmt.Printf("Key released: %s\n", keyStr)
+			delete(pressed, keyStr)
+
+			// Send stop command after key release
+			go func() {
+				_, err := ptz.DoCommand(context.Background(), map[string]interface{}{
+					"command":  "stop",
+					"pan_tilt": true,
+					"zoom":     false,
+				})
+				if err != nil {
+					logger.Error(err)
+				}
+			}()
+		})
+
+		// Stop on Escape or Ctrl+C
+		if key.Code == 27 || key.Code == 3 { // ASCII values for Escape and Ctrl+C
+			os.Exit(0)
+		}
+		return false, nil
+	})
+
+}
