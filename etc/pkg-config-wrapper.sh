6a00d5298be5042a53f517b60646911735f167e0
seanavery <seanpollock3344@gmail.com>
2025-05-15 16:27:21 -0400

    Fix pkg config wrapper path

--- etc/pkg-config-wrapper.sh
+++ etc/pkg-config-wrapper.sh
@@ -3,6 +3,6 @@
 # force pkg-config to look in your x264 build’s pkgconfig dir
 # ffmpeg configure overrides PKG_CONFIG_PATH and PKG_CONFIG_LIBDIR
 # so we need to add wrapper to inject our path
-export PKG_CONFIG_PATH=/host/x264/windows-amd64/build/lib/pkgconfig
-# now invoke the real pkg-confi
+export PKG_CONFIG_PATH="$(pwd)/x264/windows-amd64/build/lib/pkgconfig"
+# now invoke the real pkg-config
 exec /usr/bin/pkg-config "$@"
