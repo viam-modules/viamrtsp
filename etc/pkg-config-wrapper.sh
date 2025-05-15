3b9a0615bc59308c026fdf5ea284b4b204fb78c2
seanavery <seanpollock3344@gmail.com>
2025-05-15 16:39:30 -0400

    Try better path fill

--- etc/pkg-config-wrapper.sh
+++ etc/pkg-config-wrapper.sh
@@ -3,6 +3,17 @@
 # force pkg-config to look in your x264 build’s pkgconfig dir
 # ffmpeg configure overrides PKG_CONFIG_PATH and PKG_CONFIG_LIBDIR
 # so we need to add wrapper to inject our path
-export PKG_CONFIG_PATH="$(pwd)/x264/windows-amd64/build/lib/pkgconfig"
+# SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+script_path="$0"
+# Handle relative path
+case "$script_path" in
+    /*) ;; # Absolute path, do nothing
+    *) script_path="$(pwd)/$script_path" ;; # Relative path, make absolute
+esac
+SCRIPT_DIR="$(dirname "$script_path")"
+PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+export PKG_CONFIG_PATH="$PARENT_DIR/x264/windows-amd64/build/lib/pkgconfig"
+echo "Using PKG_CONFIG_PATH=$PKG_CONFIG_PATH" >&2
 # now invoke the real pkg-config
 exec /usr/bin/pkg-config "$@"
