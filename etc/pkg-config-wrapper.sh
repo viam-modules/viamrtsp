ca0d804e05c8cfb6fea93bbdf29fc46af4767f4b
seanavery <seanpollock3344@gmail.com>
2025-05-19 11:01:40 -0400

    Simplify pkg config injector

--- etc/pkg-config-wrapper.sh
+++ etc/pkg-config-wrapper.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-# force pkg-config to look at x264 build pkgconfig dir
-# ffmpeg configure overrides PKG_CONFIG_PATH and PKG_CONFIG_LIBDIR
-# so we need to add a wrapper to inject the path
-script_path="$0"
-case "$script_path" in
-    /*) ;; # Absolute path, do nothing
-    *) script_path="$(pwd)/$script_path" ;; # Relative path, make absolute
-esac
-SCRIPT_DIR="$(dirname "$script_path")"
-PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+# Force pkg-config to look at x264 build pkgconfig dir.
+# FFmpeg configure overrides PKG_CONFIG_PATH and PKG_CONFIG_LIBDIR
+# so we need to add a wrapper to inject the path at runtime.
+PARENT_DIR="$(dirname "$(dirname "$0")")"
 export PKG_CONFIG_PATH="$PARENT_DIR/x264/windows-amd64/build/lib/pkgconfig"
-echo "Using PKG_CONFIG_PATH=$PKG_CONFIG_PATH" >&2
-# now invoke the real pkg-config
+
+# Invoke the real pkg-config with the provided arguments
 exec /usr/bin/pkg-config "$@"
